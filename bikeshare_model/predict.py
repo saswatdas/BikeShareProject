a84a9e6a79670615744b4cb257ac65082d7209b1
saswatdas <35954562+saswatdas@users.noreply.github.com>
2024-02-10 09:57:33 +0530

    Update Holiday field in predit.py

--- bikeshare_model/predict.py
+++ bikeshare_model/predict.py
@@ -41,7 +41,7 @@ def make_prediction(*, input_data: Union[pd.DataFrame, dict]) -> dict:
 
 if __name__ == "__main__":
 
-    data_in = {'dteday': ['2012-11-6'], 'season': ['winter'], 'hr': ['6pm'], 'holiday': ['No'], 'weekday': ['Tue'],
+    data_in = {'dteday': ['2012-11-6'], 'season': ['winter'], 'hr': ['6pm'], 'holiday': ['Yes'], 'weekday': ['Tue'],
                'workingday': ['Yes'], 'weathersit': ['Clear'], 'temp': [16], 'atemp': [17.5], 'hum': [30], 'windspeed': [10]}
 
     make_prediction(input_data = data_in)
